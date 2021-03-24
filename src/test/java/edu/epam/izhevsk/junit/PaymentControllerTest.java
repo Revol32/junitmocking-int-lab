6a461890504c75e36a87f514a16ce635249f9daa
Revol32 <72274775+Revol32@users.noreply.github.com>
2021-03-25 02:49:03 +0400

    Cleanup code

--- src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
+++ src/test/java/edu/epam/izhevsk/junit/PaymentControllerTest.java
@@ -1,10 +1,5 @@
 package edu.epam.izhevsk.junit;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.AdditionalMatchers.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.TestInstance;
@@ -15,7 +10,12 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-import static org.mockito.Mockito.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.AdditionalMatchers.*;
+import static org.mockito.Mockito.anyLong;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PaymentControllerTest {
@@ -31,49 +31,49 @@ public class PaymentControllerTest {
         MockitoAnnotations.initMocks(this);
         Mockito.when(accountServiceMock.isUserAuthenticated(100L)).thenReturn(true);
         Mockito.when(accountServiceMock.isUserAuthenticated(not(cmpEq(100L)))).thenReturn(false);
-        Mockito.when(depositServiceMock.deposit(lt(100L),anyLong())).thenReturn("OK");
-        Mockito.when(depositServiceMock.deposit(geq(100L),anyLong())).thenThrow(new InsufficientFundsException());
+        Mockito.when(depositServiceMock.deposit(lt(100L), anyLong())).thenReturn("OK");
+        Mockito.when(depositServiceMock.deposit(geq(100L), anyLong())).thenThrow(new InsufficientFundsException());
     }
 
     @ParameterizedTest
     @DisplayName("Тест AccountService.isUserAuthenticated()")
     @MethodSource("Providers#testIsUserAuthenticatedProvider")
-    void isUserAuthenticatedTest (Long id, boolean expected) {
+    void isUserAuthenticatedTest(Long id, boolean expected) {
         assertThat(accountServiceMock.isUserAuthenticated(id), equalTo(expected));
     }
 
     @ParameterizedTest
     @DisplayName("Тест DepositService.deposit() OK")
     @MethodSource("Providers#testDepositOkProvider")
-    void depositDepositOkTest (Long id, Long amount, String expected) throws InsufficientFundsException {
-        assertThat(depositServiceMock.deposit(amount,id), equalTo(expected));
+    void depositDepositOkTest(Long id, Long amount, String expected) throws InsufficientFundsException {
+        assertThat(depositServiceMock.deposit(amount, id), equalTo(expected));
     }
 
     @ParameterizedTest
     @DisplayName("Тест DepositService.deposit() InsufficientFunds")
     @MethodSource("Providers#testDepositInsufficientFundsProvider")
-    void depositDepositInsufficientFundsTest (Long id, Long amount) throws InsufficientFundsException {
-        assertThrows(InsufficientFundsException.class, () -> depositServiceMock.deposit(amount,id));
+    void depositDepositInsufficientFundsTest(Long id, Long amount) {
+        assertThrows(InsufficientFundsException.class, () -> depositServiceMock.deposit(amount, id));
     }
 
     @ParameterizedTest
     @DisplayName("Тест PaymentController.deposit() OK")
     @MethodSource("Providers#testDepositPaymentOkProvider")
-    void depositOkTest (Long id, Long amount) {
-        assertDoesNotThrow(()->paymentController.deposit(amount,id));
+    void depositOkTest(Long id, Long amount) {
+        assertDoesNotThrow(() -> paymentController.deposit(amount, id));
     }
 
     @ParameterizedTest
     @DisplayName("Тест PaymentController.deposit() InsufficientFunds")
     @MethodSource("Providers#testDepositPaymentInsufficientFundsProvider")
-    void depositInsufficientFundsTest (Long id, Long amount) {
-        assertThrows(InsufficientFundsException.class, ()->paymentController.deposit(amount,id));
+    void depositInsufficientFundsTest(Long id, Long amount) {
+        assertThrows(InsufficientFundsException.class, () -> paymentController.deposit(amount, id));
     }
 
     @ParameterizedTest
     @DisplayName("Тест PaymentController.deposit() SecurityException")
     @MethodSource("Providers#testDepositPaymentSecurityExceptionProvider")
-    void depositSecurityExceptionTest (Long id, Long amount) {
-        assertThrows(SecurityException.class, ()->paymentController.deposit(amount,id));
+    void depositSecurityExceptionTest(Long id, Long amount) {
+        assertThrows(SecurityException.class, () -> paymentController.deposit(amount, id));
     }
 }
